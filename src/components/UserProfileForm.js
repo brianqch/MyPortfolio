6f4b63b373e00ed85cc2ad301394a76b67e4447a
Brian Quach <brianquach@berkeley.edu>
2023-04-26 21:30:54 -0700

    saving prog

---
+++ src/components/UserProfileForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function UserProfileForm() {
+    return (
+        <>
+        <div>
+            <ul>
+                <li>
+                    
+                </li>
+            </ul>
+        </div>
+        </>
+    );
+}
+
+export default UserProfileForm;
