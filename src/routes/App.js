2fffb4aa57ae589675bbfa0c924165ae367ddafe
Brian Quach <brianquach@berkeley.edu>
2023-04-28 18:21:20 -0700

    saving prog on user profile page

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './Home'
+import Home from './Home';
 import UserProfilePage from './UserProfilePage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
